b6c6aaed7ff2584c5a0a985dd6c1c5cf9923b8e9
Vlad Kalachov <vld.kalachov@gmail.com>
2019-03-16 11:45:40 +0400

    Add few view for web

---
+++ PizzeriaWebView/Controllers/PizzasController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using ForgeServiceDAL.Interfaces;
+using ForgeServiceDAL.ViewModel;
+
+namespace PizzeriaWebView.Controllers
+{
+    public class PizzasController : Controller
+    {
+        private IPizzaService service = Globals.PizzaService;
+        private IIngredientService ingredientService = Globals.IngredientService;
+
+        // GET: Pizzas
+        public ActionResult Index()
+        {
+            if (Session["Pizza"] == null)
+            {
+                var pizza = new PizzaViewModel();
+                pizza.PizzaIngredients = new List<PizzaIngredientViewModel>();
+                Session["Pizza"] = pizza;
+            }
+            return View((PizzaViewModel) Session["Pizza"]);
+        }
+
+        public ActionResult AddIngredient()
+        {
+            var ingredients = new SelectList(ingredientService.GetList(), "IngredientId", "IngredientName");
+            ViewBag.Ingredients = ingredients;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddIngredientPost()
+        {
+            var pizza = (PizzaViewModel) Session["Pizza"];
+            var ingredient = new PizzaIngredientViewModel
+            {
+                IngredientId = int.Parse(Request["IngredientId"]),
+                IngredientName = ingredientService.GetElement(int.Parse(Request["IngredientId"])).IngredientName,
+                PizzaIngredientCount = int.Parse(Request["PizzaIngredientCount"])
+            };
+            pizza.PizzaIngredients.Add(ingredient);
+            Session["Pizza"] = pizza;
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult CreatePizzaPost()
+        {
+
+        }
+    }
+}
---
+++ PizzeriaWebView/Globals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ForgeServiceDAL.Interfaces;
+using ForgeServiceDAL.ViewModel;
+using ForgeServiceImplementList.Implementations;
+
+namespace PizzeriaWebView
+{
+    public static class Globals
+    {
+        public static ICustomerService CustomerService { get; } = new CustomerServiceList();
+        public static IIngredientService IngredientService { get; } = new IngredientServiceList();
+        public static IPizzaService PizzaService { get; } = new PizzaServiceList();
+    }
+}
