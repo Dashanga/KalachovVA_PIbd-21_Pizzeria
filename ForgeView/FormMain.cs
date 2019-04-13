7aa5f018218ad88748e9761eeec98e710052a951
Vlad Kalachov <vld.kalachov@gmail.com>
2019-04-13 09:39:41 +0400

    Do lab5

--- ForgeView/FormMain.cs
+++ ForgeView/FormMain.cs
@@ -22,7 +22,7 @@ namespace ForgeView
                 if (list != null)
                 {
                     dataGridView.DataSource = list;
-                    dataGridView.Columns[0].Visible = true;
+                    dataGridView.Columns[0].Visible = false;
                     dataGridView.Columns[1].Visible = false;
                     dataGridView.Columns[3].Visible = false;
                     dataGridView.Columns[5].Visible = false;
