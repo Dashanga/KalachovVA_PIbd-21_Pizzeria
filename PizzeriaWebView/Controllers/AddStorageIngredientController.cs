25a2dbd304854710aa07f3011ed698a0af838d75
Vlad Kalachov <vld.kalachov@gmail.com>
2019-04-26 21:25:44 +0400

    Do lab2_web

---
+++ PizzeriaWebView/Controllers/AddStorageIngredientController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ForgeServiceDAL.BindingModel;
+using ForgeServiceDAL.Interfaces;
+
+namespace PizzeriaWebView.Controllers
+{
+    public class AddStorageIngredientController : Controller
+    {
+        private IIngredientService ingredientService = Globals.IngredientService;
+        private IStorageService storageService = Globals.StorageService;
+        private IPizzaOrderService mainService = Globals.PizzaOrderService;
+
+        public ActionResult Index()
+        {
+            var ingredients = new SelectList(ingredientService.GetList(), "IngredientId", "IngredientName");
+            ViewBag.Ingredients = ingredients;
+
+            var storages = new SelectList(storageService.GetList(), "StorageId", "StorageName");
+            ViewBag.Storages = storages;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddIngredientPost()
+        {
+           mainService.PutIngredientOnStorage(new StorageIngredientBindingModel
+            {
+                IngredientId = int.Parse(Request["IngredientId"]),
+                StorageId = int.Parse(Request["StorageId"]),
+                StorageIngredientCount = int.Parse(Request["StorageIngredientCount"])
+            });
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
